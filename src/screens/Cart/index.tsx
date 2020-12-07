082c2918367973a0421c03aa06521e21c54761bd
Gabriel Santos <gab_santos@outlook.com>
2020-12-07 03:42:03 -0300

    feat(mobile): create item cart

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -6,7 +6,16 @@ import CartItem from '../../components/CartItem';
 import Header from '../../components/Header';
 import AppContext from '../../contexts/appContext';
 
-import { Container, CartList } from './styles';
+import {
+  Container,
+  EmptyList,
+  CartList,
+  Foother,
+  Subtotal,
+  Shipping,
+  Total,
+  Bottom,
+} from './styles';
 
 const Cart: React.FC = () => {
   const { cartList } = useContext(AppContext);
@@ -20,11 +29,39 @@ const Cart: React.FC = () => {
       <Header back onPress={handleNavigateBack}>
         Joguinhos
       </Header>
+
+      {cartList.length <= 0 && (
+        <EmptyList>Não há item no carrinho :)</EmptyList>
+      )}
+
       <CartList
         data={cartList}
         keyExtractor={item => String(item.product.id)}
         renderItem={({ item }) => <CartItem {...{ item }} />}
       />
+
+      {cartList.length > 0 && (
+        <Foother>
+          <Subtotal.container>
+            <Subtotal.label>Subtotal: </Subtotal.label>
+            <Subtotal.info>R$ 0</Subtotal.info>
+          </Subtotal.container>
+
+          <Shipping.container>
+            <Shipping.label>Frete: </Shipping.label>
+            <Shipping.info>R$ 0</Shipping.info>
+          </Shipping.container>
+
+          <Total.container>
+            <Total.label>Total: </Total.label>
+            <Total.info>R$ 0</Total.info>
+          </Total.container>
+
+          <Bottom.container>
+            <Bottom.text>Finalizar compra</Bottom.text>
+          </Bottom.container>
+        </Foother>
+      )}
     </Container>
   );
 };
--- src/screens/Cart/styles.ts
+++ src/screens/Cart/styles.ts
@@ -1,8 +1,11 @@
 import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styled from 'styled-components/native';
 
 import { CartItem } from '../../contexts/appContext';
+
+import { metrics, colors, fonts } from '../../styles';
 
 export const Container = styled.View`
   flex: 1;
@@ -13,3 +16,82 @@ export const CartList = styled(FlatList as new () => FlatList<CartItem>).attrs({
     paddingHorizontal: 15,
   },
 })``;
+
+export const EmptyList = styled.Text`
+  ${fonts.big};
+  color: ${colors.secondary};
+  text-align: center;
+`;
+
+export const Foother = styled.View`
+  border-top-width: 1px;
+  border-color: ${colors.white};
+`;
+
+const container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 10px;
+`;
+
+export const Subtotal = {
+  container: styled(container)`
+    margin-top: ${metrics.padding.small};
+  `,
+
+  label: styled.Text`
+    ${fonts.big}
+    color: ${colors.black};
+  `,
+
+  info: styled.Text`
+    ${fonts.bigger}
+    color: ${colors.black};
+  `,
+};
+
+export const Shipping = {
+  container: styled(container)``,
+
+  label: styled.Text`
+    ${fonts.regular}
+    color: ${colors.black};
+  `,
+
+  info: styled.Text`
+    ${fonts.big}
+    color: ${colors.black};
+  `,
+};
+
+export const Total = {
+  container: styled(container)`
+    margin-bottom: ${metrics.padding.small};
+  `,
+
+  label: styled.Text`
+    ${fonts.bigger}
+    color: ${colors.primary};
+  `,
+
+  info: styled.Text`
+    ${fonts.biggest}
+    color: ${colors.primary};
+  `,
+};
+
+export const Bottom = {
+  container: styled(RectButton)`
+    height: ${metrics.height.big};
+    background-color: ${colors.primary};
+    align-items: center;
+    justify-content: center;
+  `,
+
+  text: styled.Text`
+    ${fonts.bigger}
+    color: ${colors.white};
+    text-transform: uppercase;
+  `,
+};
